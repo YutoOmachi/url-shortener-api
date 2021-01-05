0a4191891a414d837e43ad6410962e14cd00aa27
Yuto Omachi <youomachi@gmail.com>
2021-01-05 16:00:53 +0800

    good to go

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const dns = require('dns');
+const { Console } = require('console');
 
 const app = express();
 const con = mysql.createConnection({
@@ -23,66 +24,74 @@ app.get('/', (req,res)=> {
     res.sendFile(__dirname+'/views/index.html')
 })
 
+//CallBack get url short version
+function getShortURL(req,res,cb){
+    let shortVal;
+    let sql = "SELECT MAX(short) FROM urls";
+    con.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        let resutlObj = Object.assign({}, result[0]);
+        shortVal = resutlObj['MAX(short)']+1;    
+        cb(shortVal)
+    });
+}
+
 //When new URL is posted
-// app.post('/api/shorturl/new', (req,res, next)=>{
-//     let host = "";
-//     res.locals.original = req.body.url;
-//     try{    
-//         let requestURL = new URL(res.locals.original);
-//         host = requestURL.host;
-//     }
-//     catch{
-//         res.send({error: "Invalid URL"});
-//         return;
-//     }
-//     dns.lookup(host, (err,adresses)=>{
-//         if (err) {
-//             res.send({error: "Invalid URL"});
-//             return;
-//         }
-//     })
-//     next();
-// }, (req,res,next)=>{
-//     let shortVal = 1;
-//     con.connect((err)=>{
-//         if (err) throw err;
-//         let sql = "SELECT COALESCE(MAX(short),0) FROM urls";
-//         con.query(sql, function (err, result, fields) {
-//             if (err) throw err;
-//             let resutlObj = Object.assign({}, result[0]);
-//             shortVal = resutlObj['COALESCE(MAX(short),0)']+1;
-//         });
-//         sql = "INSERT INTO urls (original, short) VALUES (? , ?)"
-//         con.query(sql, [res.locals.original, shortVal], function (err, result) {
-//             if (err) throw err;
-//         });
-//     });
-//     res.locals.short = shortVal;
-//     next();
-// }, (req,res)=>{
-//     res.send({original_url: res.locals.original, short_url: res.locals.short})
-// })
-
-
-app.get("/api/shorturl/:short_url?", (req,res, next)=>{
-    let urlString = ""
-    con.connect((err)=>{
-        if(err) throw err;
-        let sql = "SELECT * FROM urls WHERE short=?";
-        con.query(sql, [req.params.short_url], (err, result, fields)=>{
-            if(err) throw err;
-            if(result){
-                let resultObj = Object.assign({}, result[0]);
-                urlString = resultObj.original;
-            }
-        })
-        console.log(urlString);
+app.post('/api/shorturl/new', (req,res, next)=>{
+    let host = "";
+    res.locals.original = req.body.url;
+    try{    
+        let requestURL = new URL(res.locals.original);
+        host = requestURL.host;
+    }
+    catch{
+        res.send({error: "Invalid URL"});
+        return;
+    }
+    dns.lookup(host, (err,adresses)=>{
+        if (err) {
+            res.send({error: "Invalid URL"});
+            return;
+        }
     })
-    res.locals.original = urlString;
     next();
 }, (req,res)=>{
-    console.log(res.locals.original);
-    res.redirect(res.locals.original);
+    con.connect((err)=>{
+        if (err) throw err;
+        getShortURL(req,res,(shortVal)=>{
+            sql = "INSERT INTO urls (original, short) VALUES (? , ?)"
+            con.query(sql, [res.locals.original, shortVal], function (err, result) {
+                if (err) throw err;
+                res.send({original_url: res.locals.original, short_url: shortVal})
+            });
+        })
+
+    });
+})
+
+
+//Gets the url and call cb
+function getURL(req,res,cb){
+    let urlString = ""
+    let sql = "SELECT * FROM urls WHERE short=?";
+    con.query(sql, [req.params.short_url], (err, result, fields)=>{
+        if(err) throw err;
+        if(result){
+            let resultObj = Object.assign({}, result[0]);
+            urlString = resultObj.original;
+        }
+        cb(urlString);
+    })
+}
+
+//Handles redirection using callback function above
+app.get("/api/shorturl/:short_url?", (req,res)=>{
+    con.connect((err)=>{
+        if(err) throw err;
+        getURL(req,res,(urlString)=>{
+            res.redirect(urlString);
+        })
+    })
 })
 
 app.listen(port, ()=>{
